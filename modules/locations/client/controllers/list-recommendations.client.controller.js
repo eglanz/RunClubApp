288ee05c85f59dc1bf0b30cf441fb69b5ecfd81c
eglanz <emily-glanz@uiowa.edu>
2016-03-25 21:52:19 +0000

    view route

--- modules/locations/client/controllers/list-recommendations.client.controller.js
+++ modules/locations/client/controllers/list-recommendations.client.controller.js
@@ -10,8 +10,13 @@
   function RecommendationsListController($state, $stateParams, RecommendationsService) {
     var vm = this;
     vm.miles = $stateParams.miles;
-
+    vm.clickview = clickview;
     vm.locations = RecommendationsService.query({ miles: vm.miles });
-
+    
+    function clickview(id){
+      $state.go('locations.view', {
+        locationId: id
+      });
+    }
   }
 })();
