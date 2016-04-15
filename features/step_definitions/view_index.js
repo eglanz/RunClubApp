8742ff640e151eac72358f9cf775425318d80daa
eglanz <emily-glanz@uiowa.edu>
2016-04-15 04:04:47 +0000

    cucumber

--- features/step_definitions/view_index.js
+++ features/step_definitions/view_index.js
@@ -6,19 +6,21 @@ module.exports = function(){
     });
     
     this.When(/^I go to the sign up url$/, function(callback){
-        this.browser.click('#singuplink',callback);
+        this.browser.click('#signuplink',callback);
     });
     
     
     this.When(/^I sign up$/, function(callback){
-        this.browser.
-      setValue('input#username', 'user'+Math.random()).
-      setValue('input#firstName', 'Zener').
+        //this.browser.setValue('input#username', 'user', callback);
+        //this.browser.setValue('input#username', 'user');
+        this.browser.window.setValue('#input#username', 'user');
+      //setValue('input#username', 'user'+Math.random()).
+      /*setValue('input#firstName', 'Zener').
       setValue('input#lastName', 'Diode').
       setValue('input#email', 'zener@email.com').
       setValue('input#password', '1234567890abcdefjklmno').
       submitForm('#userForm')
-      //.call(callback)
+      .call(callback)*/
     });
     
     this.Then(/^I should see "(.*)"$/, function(title, callback){
